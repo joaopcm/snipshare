c0f6ad1299b2f4eeb1051b26a551bbd7c75447ba
João Melo <jopcmelo@gmail.com>
2023-02-24 13:07:29 -0300

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Analytics } from "@vercel/analytics/react";
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Analytics />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    </>
+  );
 }
