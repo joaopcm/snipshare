81ab53ec1ac98eb7f950c7209deb731f871ab3dc
João Melo <jopcmelo@gmail.com>
2023-02-24 12:39:13 -0300

    feat: clear the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { App } from "./App";
+
+import "./styles/global.css";
+
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
