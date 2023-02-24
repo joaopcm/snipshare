5e9f7779de3c741cb90a46ee0fa347375b6a5e7c
João Melo <jopcmelo@gmail.com>
2023-02-24 16:14:39 -0300

    fix: add cors to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    headers: {
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Resource-Policy": "cross-origin",
-    },
+    cors: true,
+    // headers: {
+    //   "Cross-Origin-Embedder-Policy": "require-corp",
+    //   "Cross-Origin-Opener-Policy": "same-origin",
+    //   "Cross-Origin-Resource-Policy": "cross-origin",
+    // },
   },
 });
