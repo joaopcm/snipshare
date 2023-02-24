74a38c1c5e56444b158594c0e9da1b6fd87fbbc6
João Melo <jopcmelo@gmail.com>
2023-02-24 16:08:45 -0300

    fix: add all the 3 http response headers

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Resource-Policy": "cross-origin",
     },
   },
 });
