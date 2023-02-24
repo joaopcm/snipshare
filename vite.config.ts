9acf7c72cdd4860b86b10600bdfb03acb76b98ec
João Melo <jopcmelo@gmail.com>
2023-02-24 16:40:40 -0300

    fix: remove the plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crossOriginIsolation()],
-  // server: {
-  //   headers: {
-  //     "Cross-Origin-Embedder-Policy": "require-corp",
-  //     "Cross-Origin-Opener-Policy": "same-origin",
-  //   },
-  // },
+  plugins: [react()],
+  server: {
+    https: true,
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 });
