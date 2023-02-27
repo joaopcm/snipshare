79b528a1ae0adf9fc134ee7fde713f0a35a58fcf
João Melo <jopcmelo@gmail.com>
2023-02-27 16:10:45 -0300

    feat: add next-seo setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import Head from 'next/head'
+import { DefaultSeo } from 'next-seo'
+import { SEOConfig } from '@/config/seo.config'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <DefaultSeo {...SEOConfig} />
       <Component {...pageProps} />
       <Analytics />
     </>
