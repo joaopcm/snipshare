b4653945f7d974438862a5dcf1b4596441c006b1
João Melo <jopcmelo@gmail.com>
2023-03-19 19:49:52 -0300

    fix: remove viewport from _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
