1d306f49ebb8af98e8ce089982b093b4b3bd8512
João Melo <jopcmelo@gmail.com>
2023-03-20 22:48:40 -0300

    fix: fix lint-staged rc file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "**/*.js?(x)": (filenames) =>
+  "**/*.{js,jsx,ts,tsx}": (filenames) =>
     `next lint --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(" --file ")}`,
