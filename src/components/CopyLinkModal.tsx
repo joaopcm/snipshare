15a010d8e34fb513e30d4e5ffcf4318413415521
João Melo <jopcmelo@gmail.com>
2023-03-23 12:13:13 -0300

    fix(copy-link-modal): make the CopyLinkModal component larger to render the full generated URL

--- src/components/CopyLinkModal.tsx
+++ src/components/CopyLinkModal.tsx
@@ -46,7 +46,7 @@ export function CopyLinkModal({ link, onClose }: CopyLinkModalProps) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-omni-light px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-omni-light px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-6">
                 <div>
                   <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-emerald-500">
                     <Copy
