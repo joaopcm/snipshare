073033d009f34f291c91e10383b7c9a55cd1c919
João Melo <jopcmelo@gmail.com>
2024-04-23 17:08:34 -0300

    chore: delete the entire app

--- src/components/Editor.tsx
+++
@@ -1,9 +0,0 @@
-import { EditorContent } from '@tiptap/react'
-
-import { useEditor } from '@/contexts/EditorContext'
-
-export function Editor() {
-  const { editor } = useEditor()
-
-  return <EditorContent editor={editor} spellCheck={false} />
-}
