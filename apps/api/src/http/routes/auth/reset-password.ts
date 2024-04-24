2b449031d8c3b23d6eeaada24110dff90f1bfe31
João Melo <jopcmelo@gmail.com>
2024-04-24 19:10:29 -0300

    fix(apps/api): enable cascading references in the database

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -37,15 +37,17 @@ export async function resetPassword(app: FastifyInstance) {
       }
 
       const passwordHash = await hash(password, 6)
-      await prisma.user.update({
-        where: {
-          id: tokenFromCode.userId,
-        },
-        data: {
-          passwordHash,
-        },
-      })
-      await prisma.token.delete({ where: { id: code } })
+      await prisma.$transaction([
+        prisma.user.update({
+          where: {
+            id: tokenFromCode.userId,
+          },
+          data: {
+            passwordHash,
+          },
+        }),
+        prisma.token.delete({ where: { id: code } }),
+      ])
 
       return reply.status(204).send()
     },
