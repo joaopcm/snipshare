ac0cd70713a37a40cb2a3b6a736e2dfec84f966e
João Melo <jopcmelo@gmail.com>
2024-04-24 17:18:36 -0300

    feat(apps/api): add an endpoint to list memberships

---
+++ apps/api/src/http/routes/memberships/get-members.ts
@@ -0,0 +1,94 @@
+import { Role } from '@prisma/client'
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getMembers(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/members',
+      {
+        schema: {
+          tags: ['members'],
+          summary: 'Get all organization members',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              members: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  userId: z.string().uuid(),
+                  role: z.nativeEnum(Role),
+                  name: z.string().nullable(),
+                  email: z.string().email(),
+                  avatarUrl: z.string().url().nullable(),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { slug } = request.params
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+        if (cannot('get', 'User')) {
+          throw new UnauthorizedError(
+            "You don't have permission to see organization members.",
+          )
+        }
+
+        const members = await prisma.membership.findMany({
+          select: {
+            id: true,
+            role: true,
+            user: {
+              select: {
+                id: true,
+                name: true,
+                email: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            organizationId: organization.id,
+          },
+          orderBy: [
+            {
+              role: 'asc',
+            },
+            {
+              user: {
+                name: 'asc',
+              },
+            },
+          ],
+        })
+
+        const membersWithRoles = members.map(
+          ({ user: { id: userId, ...user }, ...membership }) => ({
+            ...user,
+            ...membership,
+            userId,
+          }),
+        )
+
+        return reply.send({ members: membersWithRoles })
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { getMembers } from './routes/memberships/get-members'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
@@ -83,6 +84,7 @@ app.register(deleteProject)
 app.register(getProject)
 app.register(getProjects)
 app.register(updateProject)
+app.register(getMembers)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
