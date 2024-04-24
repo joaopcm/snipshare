09d276cd2caf7c8756948657fe71a7bd10ec4273
João Melo <jopcmelo@gmail.com>
2024-04-24 16:21:39 -0300

    feat(apps/api): add an endpoint to update organizations

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
+import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -67,6 +68,7 @@ app.register(createOrganization)
 app.register(getMembership)
 app.register(getOrganizations)
 app.register(getOrganization)
+app.register(updateOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
