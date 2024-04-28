e68ee70616f5c965f6dff70d144cf3d692547977
João Melo <jopcmelo@gmail.com>
2024-04-28 10:11:36 -0300

    feat(apps/storybook): improve the styled example

--- apps/storybook/src/components/my-code-block.tsx
+++ apps/storybook/src/components/my-code-block.tsx
@@ -6,7 +6,7 @@ import {
   useCodeEditor,
 } from '@snipshare/code-block'
 import { Loader2, RocketIcon } from 'lucide-react'
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 
 import { cn } from '@/lib/utils'
 
@@ -29,14 +29,7 @@ export const MyCodeBlock: React.FC<MyCodeBlockProps> = ({
   styled = false,
   initialCode,
 }) => {
-  const { isRunning, output, code } = useCodeEditor()
-  const editorColumnRef = useRef<HTMLDivElement>(null)
-  const [terminalHeight, setTerminalHeight] = useState<string>('auto')
-
-  useEffect(() => {
-    if (!editorColumnRef.current) return
-    setTerminalHeight(`${editorColumnRef.current.clientHeight}px`)
-  }, [editorColumnRef, code])
+  const { isRunning, output } = useCodeEditor()
 
   if (!styled) {
     return (
@@ -51,9 +44,62 @@ export const MyCodeBlock: React.FC<MyCodeBlockProps> = ({
   }
 
   return (
+    <div className="flex h-[600px] flex-1 gap-4">
+      <div className="flex-1 rounded-3xl border border-zinc-700 bg-primary px-8 shadow-inner">
+        <CodeBlock.Editor
+          initialCode={initialCode}
+          options={{
+            cursorSmoothCaretAnimation: 'on',
+            lineDecorationsWidth: 0,
+            lineNumbers: 'off',
+            fontSize: 16,
+            padding: {
+              top: 32,
+              bottom: 32,
+            },
+            scrollbar: {
+              vertical: 'hidden',
+            },
+            cursorBlinking: 'smooth',
+            renderLineHighlight: 'none',
+          }}
+          theme={editorTheme}
+        />
+
+        <CodeBlock.ControlButton
+          className={cn(
+            buttonVariants({
+              variant: isRunning ? 'destructive' : 'default',
+            }),
+            'relative -right-2 -top-16 float-end rounded-2xl',
+          )}
+        >
+          {isRunning ? (
+            <Loader2 className="mr-3 h-4 w-4 animate-spin" />
+          ) : (
+            <RocketIcon className="mr-3 h-4 w-4" />
+          )}
+
+          {isRunning ? 'Stop running' : 'Run code'}
+        </CodeBlock.ControlButton>
+      </div>
+
+      <div className="flex w-1/3 flex-col overflow-auto whitespace-pre rounded-3xl bg-black p-4 font-mono text-xs text-zinc-400">
+        {output.map((line, index) => (
+          <span
+            key={`${line}-${index}`}
+            dangerouslySetInnerHTML={{ __html: line }}
+            className="block"
+          />
+        ))}
+      </div>
+    </div>
+  )
+
+  return (
     <div className="flex min-h-[600px] flex-1 gap-4">
-      <div className="flex flex-1 flex-col gap-4" ref={editorColumnRef}>
-        <div className="flex flex-1 overflow-y-scroll rounded-3xl border border-zinc-700 bg-primary px-8 shadow-inner">
+      <div className="flex flex-1 flex-col gap-4">
+        <div className="flex-1 rounded-3xl border border-zinc-700 bg-primary px-8 shadow-inner">
           <CodeBlock.Editor
             initialCode={initialCode}
             options={{
@@ -69,37 +115,32 @@ export const MyCodeBlock: React.FC<MyCodeBlockProps> = ({
                 vertical: 'hidden',
               },
               cursorBlinking: 'smooth',
+              renderLineHighlight: 'none',
             }}
             theme={editorTheme}
           />
+
+          <CodeBlock.ControlButton
+          // className={cn(
+          //   buttonVariants({
+          //     variant: isRunning ? 'destructive' : 'default',
+          //     size: 'lg',
+          //   }),
+          //   'rounded-3xl',
+          // )}
+          >
+            {isRunning ? (
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              <RocketIcon className="mr-2 h-4 w-4" />
+            )}
+
+            {isRunning ? 'Stop running' : 'Run code'}
+          </CodeBlock.ControlButton>
         </div>
-
-        <CodeBlock.ControlButton
-          className={cn(
-            buttonVariants({
-              variant: isRunning ? 'destructive' : 'default',
-              size: 'lg',
-            }),
-            'rounded-3xl',
-          )}
-        >
-          {isRunning ? (
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <RocketIcon className="mr-2 h-4 w-4" />
-          )}
-
-          {isRunning ? 'Stop running' : 'Run code'}
-        </CodeBlock.ControlButton>
       </div>
 
-      <div
-        className="flex w-1/3 flex-col overflow-auto whitespace-pre rounded-3xl bg-black p-4 font-mono text-xs text-zinc-400"
-        style={{
-          maxHeight: terminalHeight,
-          overflow: 'auto',
-        }}
-      >
+      <div className="flex w-1/3 flex-col overflow-auto whitespace-pre rounded-3xl bg-black p-4 font-mono text-xs text-zinc-400">
         {output.map((line, index) => (
           <span
             key={`${line}-${index}`}
--- apps/storybook/tailwind.config.js
+++ apps/storybook/tailwind.config.js
@@ -58,6 +58,7 @@ module.exports = {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
+        '4xl': '2rem',
       },
       keyframes: {
         'accordion-down': {
