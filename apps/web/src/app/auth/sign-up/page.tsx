32ef8b8b355a781ca1f6711746ceda9a759cef24
João Melo <jopcmelo@gmail.com>
2024-06-07 14:15:21 -0300

    feat: add auth pages

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,9 +7,14 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
+
       <div className="space-y-1">
         <Label htmlFor="email">Email</Label>
         <Input name="email" type="email" id="email" />
@@ -18,21 +23,23 @@ export default function SignInPage() {
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
+      </div>
 
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
+      <div className="space-y-1">
+        <Label htmlFor="password-confirmation">Confirm your password</Label>
+        <Input
+          name="password-confirmation"
+          type="password"
+          id="password-confirmation"
+        />
       </div>
 
       <Button type="submit" className="w-full">
-        Sign in with email
+        Create account
       </Button>
 
       <Button className="w-full" variant="link" size="sm" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
       </Button>
 
       <Separator />
@@ -44,7 +51,7 @@ export default function SignInPage() {
           priority
           alt=""
         />
-        Sign in with GitHub
+        Sign up with GitHub
       </Button>
     </form>
   )
