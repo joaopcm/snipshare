d2086149c3126e1d8f3150befad962649f3a08ab
João Melo <jopcmelo@gmail.com>
2024-06-09 19:51:24 -0300

    feat(apps/web): add theme switcher

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
+import { ThemeProvider } from 'next-themes'
 import type { PropsWithChildren } from 'react'
 
 export const metadata: Metadata = {
@@ -9,8 +10,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
-    <html lang="en" className="dark">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
